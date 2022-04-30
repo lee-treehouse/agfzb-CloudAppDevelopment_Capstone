8031aae91a4fe91e8ba256e78017798b36257915
lee-treehouse <leentaylor@gmail.com>
2022-04-30 14:57:46 +1000

    display something from the serverless function

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     # path for logout
     path('logout/', views.logout_request, name='logout'),
 
-    path('get_dealerships/', view=views.get_dealerships, name='index'),
+    path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
 
